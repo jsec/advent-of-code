7a255515d23fb6da5257a36442fb2cde78b29065
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-12-07 10:57:06 -0500

    (2025) boilerplate for remaining days

---
+++ 2025/puzzles/day_7.py
@@ -0,0 +1,12 @@
+from typing import override
+from util.solver import Solver
+
+
+class Day7(Solver):
+    @override
+    def solve_part_1(self) -> int:
+        return 0
+
+    @override
+    def solve_part_2(self) -> int:
+        return 0
---
+++ 2025/puzzles/day_8.py
@@ -0,0 +1,12 @@
+from typing import override
+from util.solver import Solver
+
+
+class Day8(Solver):
+    @override
+    def solve_part_1(self) -> int:
+        return 0
+
+    @override
+    def solve_part_2(self) -> int:
+        return 0
